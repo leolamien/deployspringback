398418b3f9c73caf8307f9dfcb9beb50aac19007
leolamien <sienou.lamien@student.vinci.be>
2022-12-11 21:34:54 +0100

    add route

--- src/main/java/com/example/kickerdavinci/Repository/RankingsRepository.java
+++ src/main/java/com/example/kickerdavinci/Repository/RankingsRepository.java
@@ -2,9 +2,10 @@ package com.example.kickerdavinci.Repository;
 
 import com.example.kickerdavinci.Models.Ranking;
 import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RankingsRepository extends CrudRepository<Ranking, Long> {
+public interface RankingsRepository extends JpaRepository<Ranking, Long> {
 
   boolean existsByName(String name);
 
