ad46afabc39f4b2c36507e820cf4b777f181a1d2
leolamien <sienou.lamien@student.vinci.be>
2022-12-13 12:06:23 +0100

    pipeline

--- src/main/java/com/example/kickerdavinci/Controllers/UserController.java
+++ src/main/java/com/example/kickerdavinci/Controllers/UserController.java
@@ -7,6 +7,7 @@ import com.example.kickerdavinci.Services.UserService;
 import java.time.LocalDate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin(origins = { "https://kickerdavinci.azurewebsites.net" })
 @RestController
 public class UserController {
 
--- src/main/java/com/example/kickerdavinci/KickerDaVinciApplication.java
+++ src/main/java/com/example/kickerdavinci/KickerDaVinciApplication.java
@@ -14,19 +14,4 @@ public class KickerDaVinciApplication {
   public static void main(String[] args) {
     SpringApplication.run(KickerDaVinciApplication.class, args);
   }
-  @Bean
-    public CorsFilter corsFilter() {
-      CorsConfiguration corsConfiguration = new CorsConfiguration();
-      corsConfiguration.setAllowCredentials(true);
-      corsConfiguration.setAllowedOrigins(Arrays.asList("https://kickerdavinci.azurewebsites.net"));
-      corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-          "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-          "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-      corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-          "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-      corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-      UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-      urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-      return new CorsFilter(urlBasedCorsConfigurationSource);
-    }
 }
